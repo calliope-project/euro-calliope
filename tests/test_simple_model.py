6de868ecb1105af275bce6d660403dd8306eeb25
Tim Tröndle <tim.troendle@usys.ethz.ch>
2018-10-17 17:40:49 +0200

    Add simple model including test

---
+++ tests/test_simple_model.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+import calliope
+
+PATH_TO_SIMPLE_MODEL = Path(__file__).parent / "simple-model.yaml"
+
+
+def test_simple_model_runs():
+    model = calliope.Model(PATH_TO_SIMPLE_MODEL.as_posix())
+    model.run()
