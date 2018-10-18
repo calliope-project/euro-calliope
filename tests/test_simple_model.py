74849017c8518cca750ca898d2edd3fe2e9a78b3
Tim Tröndle <tim.troendle@usys.ethz.ch>
2018-10-18 13:59:19 +0200

    Fix renewable resource

--- tests/test_simple_model.py
+++ tests/test_simple_model.py
@@ -8,3 +8,4 @@ PATH_TO_SIMPLE_MODEL = Path(__file__).parent / "simple-model.yaml"
 def test_simple_model_runs():
     model = calliope.Model(PATH_TO_SIMPLE_MODEL.as_posix())
     model.run()
+    assert model.results.termination_condition == "optimal"
