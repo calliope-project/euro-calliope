c363ae2349abe60e45fda06352eebc3e4ca41c2f
Tim Tröndle <tim.troendle@usys.ethz.ch>
2019-02-28 14:31:38 +0100

    Update from kW to MW
    This is to avoid numerical issues. In any case, MW and MWh seem the more appropriate units when dealing with all Europe.

--- src/locations.py
+++ src/locations.py
@@ -4,10 +4,10 @@ import pandas as pd
 
 FLAT_ROOF_SHARE = 0.302 # TODO add source (own publication)
 MAXIMUM_INSTALLABLE_POWER_DENSITY = {
-    "pv-on-tilted-roofs": 160, # [W/m^2] from (Gagnon:2016, Klauser:2016), i.e. 16% efficiency
-    "pv-on-flat-areas": 80, # [W/m^2] from (Gagnon:2016, Klauser:2016, Wirth:2017) # FIXME also defined in renewable-techs.yaml
-    "onshore-wind": 8, # [W/m^2] from (European Environment Agency, 2009) # FIXME also defined in renewable-techs.yaml
-    "offshore-wind": 15 # [W/m^2] from (European Environment Agency, 2009)
+    "pv-on-tilted-roofs": 160, # [MW/km^2] from (Gagnon:2016, Klauser:2016), i.e. 16% efficiency
+    "pv-on-flat-areas": 80, # [MW/km^2] from (Gagnon:2016, Klauser:2016, Wirth:2017) # FIXME also defined in renewable-techs.yaml
+    "onshore-wind": 8, # [MW/km^2] from (European Environment Agency, 2009) # FIXME also defined in renewable-techs.yaml
+    "offshore-wind": 15 # [MW/km^2] from (European Environment Agency, 2009)
 }
 
 TEMPLATE = """locations:
@@ -20,17 +20,17 @@ TEMPLATE = """locations:
             wind_onshore_competing:
     {% for country, eligibility in eligibilities.iterrows() %}
     {{ country }}:
-        available_area: {{ eligibility.eligibility_onshore_wind_and_pv_km2 }} # usable by onshore wind or open field pv
+        available_area: {{ eligibility.eligibility_onshore_wind_and_pv_km2 }} # [km2] usable by onshore wind or open field pv
         techs:
             wind_onshore_monopoly:
                 constraints:
-                    energy_cap_max: {{ eligibility.eligibility_onshore_wind_monopoly_kw }}
+                    energy_cap_max: {{ eligibility.eligibility_onshore_wind_monopoly_mw }} # [MW]
             roof_mounted_pv:
                 constraints:
-                    energy_cap_max: {{ eligibility.eligibility_rooftop_pv_kw }}
+                    energy_cap_max: {{ eligibility.eligibility_rooftop_pv_mw }} # [MW]
             wind_offshore:
                 constraints:
-                    energy_cap_max: {{ eligibility.eligibility_offshore_wind_kw }}
+                    energy_cap_max: {{ eligibility.eligibility_offshore_wind_mw }} # [MW]
     {% endfor %}
 """
 
@@ -55,12 +55,12 @@ def _from_area_to_installed_capacity(land_eligibiligy_km2, flat_roof_share,
     factor_rooftop = (
         maximum_installable_power_density["pv-on-flat-areas"] * flat_roof_share
         + maximum_installable_power_density["pv-on-tilted-roofs"] * (1 - flat_roof_share)
-    ) * 1000 # MW/km2 to kW/km2
-    factor_onshore = maximum_installable_power_density["onshore-wind"] * 1000 # MW/km2 to kW/km2
-    factor_offshore = maximum_installable_power_density["offshore-wind"] * 1000 # MW/km2 to kW/km2
-    cap["eligibility_rooftop_pv_kw"] = cap["eligibility_rooftop_pv_km2"] * factor_rooftop
-    cap["eligibility_offshore_wind_kw"] = cap["eligibility_offshore_wind_km2"] * factor_offshore
-    cap["eligibility_onshore_wind_monopoly_kw"] = cap["eligibility_onshore_wind_km2"] * factor_onshore
+    )
+    factor_onshore = maximum_installable_power_density["onshore-wind"]
+    factor_offshore = maximum_installable_power_density["offshore-wind"]
+    cap["eligibility_rooftop_pv_mw"] = cap["eligibility_rooftop_pv_km2"] * factor_rooftop
+    cap["eligibility_offshore_wind_mw"] = cap["eligibility_offshore_wind_km2"] * factor_offshore
+    cap["eligibility_onshore_wind_monopoly_mw"] = cap["eligibility_onshore_wind_km2"] * factor_onshore
     return cap
 
 
