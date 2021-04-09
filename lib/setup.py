fc7370e75e7345d90da82c5e0c7fbbcb4e46a5e4
Tim Tröndle <tim.troendle@usys.ethz.ch>
2021-04-09 19:06:48 +0200

    Add eurocalliopelib as editable dependency and add its version to build-metadata

--- lib/setup.py
+++ lib/setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='eurocalliopelib',
-    version='0.1.0',
+    version='0.1.0', # additionally defined in __init__.py
     description='Library code of the euro-calliope workflow.',
-    maintainer='Tim Tröndle',
+    maintainer='calliope-project',
     maintainer_email='tim.troendle@usys.ethz.ch',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
-    install_requires=["pycountry"],
+    install_requires=["pycountry>=18.12.8"],
     classifiers=[
         'Environment :: Console',
         'Intended Audience :: Science/Research',
