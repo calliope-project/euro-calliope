b1fc513f0119efe3e641e1abeee3aa2dfefb6ff0
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2024-04-09 10:54:25 +0200

    Improve script structure, docstring, typing

--- scripts/ch-stats/annual_energy_balance.py
+++ scripts/ch-stats/annual_energy_balance.py
@@ -1,10 +1,23 @@
 import pandas as pd
+import xarray as xr
 from eurocalliopelib import utils
 
+CH_ENERGY_CARRIERS = {
+    "Erdölprodukte": "O4000XBIO",
+    "Elektrizität": "E7000",
+    "Gas": "G3000",
+    "Kohle": "C0000X0350-0370",
+    "Holzenergie": "R5110-5150_W6000RI",
+    "Fernwärme": "H8000",
+    "Industrieabfälle": "W6100_6220",
+    "Übrige erneuerbare Energien": "RA000",
+    "Total\n= %": "TOTAL",
+}
 
-def ch_energy_balance(path_to_ch_energy_balance_excel, path_to_output):
+
+def ch_energy_balance(path_to_ch_energy_balance_excel: str, path_to_output: str):
     """
-    Process Swiss data into a tidy dataframe that matches the structure of the annual energy balances.
+    Process Swiss data into an xarray dataset that matches the structure of the annual energy balances.
     """
     ch_hh_energy_use = get_ch_energy_balance_excel_sheet(
         path_to_ch_energy_balance_excel, "T17a", skipfooter=9, cat_code="FC_OTH_HH_E"
@@ -16,37 +29,33 @@ def ch_energy_balance(path_to_ch_energy_balance_excel, path_to_output):
         path_to_ch_energy_balance_excel, "T17c", skipfooter=12, cat_code="FC_OTH_CP_E"
     )
 
-    ch_energy_use_da = utils.merge_da([
-        ch_hh_energy_use,
-        ch_ind_energy_use,
-        ch_ser_energy_use,
-    ])
+    ch_energy_use_da = utils.merge_da(
+        [
+            ch_hh_energy_use,
+            ch_ind_energy_use,
+            ch_ser_energy_use,
+        ]
+    )
 
     ch_energy_use_da.expand_dims(country_code=["CHE"]).to_netcdf(path_to_output)
 
 
-def get_ch_energy_balance_excel_sheet(path_to_excel, sheet, skipfooter, cat_code):
+def get_ch_energy_balance_excel_sheet(
+    path_to_excel: str, sheet: str, skipfooter: int, cat_code: str
+) -> xr.DataArray:
     """
     Get energy balance data per sector, which requires translating energy carrier
     names and removing footnotes that are in the spreadsheet
 
-    Parameters
-    ----------
-    sheet : sheet name in the excel
-    skipfooter : number of footer rows to ignore which correspond to footnotes in the sheet
-    cat_code : sector (category) for which the sheet has data, corresponding to eurostat sectors.
+    Args:
+        path_to_excel (str): Path to CH annual energy balance Excel.
+        sheet (str): sheet name in the Excel.
+        skipfooter (int): number of footer rows to ignore which correspond to footnotes in the sheet.
+        cat_code (str): sector (category) for which the sheet has data, corresponding to eurostat sectors.
+
+    Returns:
+        xr.DataArray: Processed array.
     """
-    ch_energy_carriers = {
-        "Erdölprodukte": "O4000XBIO",
-        "Elektrizität": "E7000",
-        "Gas": "G3000",
-        "Kohle": "C0000X0350-0370",
-        "Holzenergie": "R5110-5150_W6000RI",
-        "Fernwärme": "H8000",
-        "Industrieabfälle": "W6100_6220",
-        "Übrige erneuerbare Energien": "RA000",
-        "Total\n= %": "TOTAL",
-    }
     # Footnote labels lead to some strings randomly ending in numbers; we remove them here
     df = (
         pd.read_excel(
@@ -60,12 +69,12 @@ def get_ch_energy_balance_excel_sheet(path_to_excel, sheet, skipfooter, cat_code
         # Ignore columns giving % use
         .xs("TJ", level=-1, axis=1)
         # ignore the column giving subset of oil use which is light oil
-        .iloc[:, [i for i in range(10) if i != 1]]
+        .drop("davon Heizöl extra-leicht", level=1, axis=1)
     )
     df.columns = (
         df.columns.get_level_values(0)
         .str.translate(utils.remove_digits())
-        .map(ch_energy_carriers)
+        .map(CH_ENERGY_CARRIERS)
         .rename("carrier_code")
     )
     df.index.rename("year", inplace=True)
