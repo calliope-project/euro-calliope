8e5748048c66c9a52a6b916eaf693480e9b94cdc
Tim Tröndle <tim.troendle@usys.ethz.ch>
2024-06-01 11:06:44 +0200

    Update schema CI test to allow for validating parts of config

--- tests/validate_schema.py
+++ tests/validate_schema.py
@@ -12,6 +12,13 @@ parser.add_argument(
         "If not given, schema itself will be validated against JSON schema Draft 2020-12"
     ),
 )
+parser.add_argument(
+    "--level",
+    help=(
+        "level of the configuration to validate. "
+        "If not given, this will validate the entire configuration."
+    ),
+)
 args = parser.parse_args()
 
 with open(args.schema) as f:
@@ -20,6 +27,8 @@ with open(args.schema) as f:
 if args.config:
     with open(args.config) as f:
         config = yaml.safe_load(f)
+    if args.level:
+        config = config[args.level]
     jsonschema.validate(config, schema)
 else:
     jsonschema.Draft202012Validator.check_schema(schema)
